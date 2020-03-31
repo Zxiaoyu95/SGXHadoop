9a6c4d7dbc03683c0f44b0316773399f30289a75
Zxiaoyu95 <884823258@qq.com>
2020-03-31 11:15:29 +0800

    0331

---
+++ linux-sgx-driver-master/install.sh
@@ -0,0 +1,11 @@
+sudo /sbin/modprobe -r isgx
+sudo rm -rf "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
+sudo /sbin/depmod
+sudo /bin/sed -i '/^isgx$/d' /etc/modules
+make clean
+make
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
+sudo /sbin/depmod
+sudo /sbin/modprobe isgx
